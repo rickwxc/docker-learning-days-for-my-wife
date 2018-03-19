02beb4a75ac5d4a54c71b2819b11ece532a05650
rick <lurker2000wxc@gmail.com>
2018-03-19 15:07:26 +1100

    volume readonly update

--- day1-docker-compose/python-flask-volumn-config/web/app.py
+++ day1-docker-compose/python-flask-volumn-config/web/app.py
@@ -4,6 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return ('Hello, Flask and my name is ' + os.environ["ENV_MY_NAME"]  + ' and flask debug = ' + os.environ["FLASK_DEBUG"]  )
+    debug = '0'
+    if os.environ.get('FLASK_DEBUG'):
+        debug = '1'
+
+    return ('Hello, Flask and my name is ' + os.environ["ENV_MY_NAME"]  + ' and flask debug = ' + debug)
 
 
