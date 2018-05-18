63f74fb59bfe1a3c6a6f403f0bdc3053cdbaab38
rick <lurker2000wxc@gmail.com>
2018-05-18 11:23:17 +1000

    geth run

--- day6-eth/geth/2run.sh
+++ day6-eth/geth/2run.sh
@@ -1,2 +1,5 @@
 #docker run -it  --entrypoint "geth"  --rm  --name day6_geth day6_geth   attach http://localhost:8545
-docker run -it  --entrypoint "tail"  --rm  --name day6_geth day6_geth     -f /dev/null 
+docker run -it -p 8545:8545 --entrypoint "tail"  --rm  --name day6_geth day6_geth     -f /dev/null 
+
+#geth --dev --rpcapi "db,eth,net,web3,personal" --rpc  --rpcaddr "0.0.0.0" --rpccorsdomain "*" 
+
